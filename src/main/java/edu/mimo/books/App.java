bddce33507c95cf436114833d87e677ce8d73301
Laabidi RAISSI <laabidiraissi@gmail.com>
2025-06-14 00:27:31 +0200

    Prepare exercise

--- src/main/java/edu/mimo/books/App.java
+++ src/main/java/edu/mimo/books/App.java
@@ -1,9 +1,45 @@
 package edu.mimo.books;
 
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
 
+        YOUR_CODE_HERE personRepository = new YOUR_CODE_HERE();
+
+        List<YOUR_CODE_HERE> persons = personRepository.YOUR_CODE_HERE;
+
+        for(YOUR_CODE_HERE) {
+            System.out.println(YOUR_CODE_HERE);
+        }
+        /*Doit afficher exactement ces 6 lignes:
+        --------
+        My name is Sarah Johnson, my role is "teacher".  
+        My name is Michael Brown, my role is "admin".  
+        My name is Emma Davis, my role is "student".  
+        My name is Liam Wilson, my role is "student".  
+        --------
+        */
+
+        List<YOUR_CODE_HERE> staff = personRepository.YOUR_CODE_HERE;
+
+        for(YOUR_CODE_HERE) {
+            if(YOUR_CODE_HERE) {
+                int bonus = YOUR_CODE_HERE;
+                System.out.println(YOUR_CODE_HERE);
+            } else if (YOUR_CODE_HERE) {
+                int bonus = YOUR_CODE_HERE;
+                System.out.println(YOUR_CODE_HERE);
+            }
+        }
+
+        /*Doit afficher exactement ces 4 lignes:
+        --------
+        Teacher Sarah Johnson will get a bonus = 600.  
+        Admin Michael Brown will get a bonus = 400. 
+        --------
+        */
 
 
     }
